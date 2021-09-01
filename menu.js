73f4dce6e3bccfd8b66b1e05808e59db8dd3577b
Mariana <marianaufc@gmail.com>
2021-08-31 21:29:46 -0300

    Changed lateral menu, added active class

--- menu.js
+++ menu.js
@@ -2,9 +2,5 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.lateral-menu');
 
 hamburger.addEventListener('click', () => {
-    if (menu.style.display == "none") {
-        menu.style.display = "block";
-    } else {
-        menu.style.display = "none";
-    }
+    menu.classList.toggle('active');
 })
